fac9dd1e0ca0ed41725ed31ba49c9f85f5e17ab9
michiya0009 <javatea0009@gmail.com>
2015-08-17 06:31:12 +0000

    using Bootstrap

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -15,7 +15,7 @@ class MessagesController < ApplicationController
   def message_params
     # params[:message]のパラメータでname, bodyのみ許可する。
     # 返り値は　ex:) {name: "入力されたname" , body: "入力されたbody" }
-    params.require(:message).permit(:name, :body)
+    params.require(:message, :comtent, :content).permit(:name, :body ,:comtent, :content)
   end
   ## ここまで
 end
